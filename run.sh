3ac0b90ea97fd3efbea5e13d81ca18254b1352cb
Christophe Prieur <prieur@liafa.fr>
2014-08-01 12:29:52 +0200

    backup.csv

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ resdir=Resultats
 csv=$resdir/indicators.csv
 
 test -d $resdir || mkdir $resdir
-test -f $csv && mv -i $csv old-$csv
+test -f $csv && mv -i $csv $resdir/backup.csv
 echo 'identifiant ego;;nb amis;nb liens;nb com Louvain;nb amis max CC;nb com Louvain max CC;diametre;nb amis isoles;modularite;coeff clustering;densite' > $csv
 
 for ego in $data/????????????????????????????
