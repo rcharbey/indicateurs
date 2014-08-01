126c15e236270c80b0d9fa1bd41f65df1d133260
Raphaël <r.charbey@gmail.com>
2014-08-01 11:58:54 +0200

    ligne d'en-têtes du csv + qq params

--- run.sh
+++ run.sh
@@ -7,6 +7,13 @@ else
   data=.
 fi
 
+# Attention, ces paramètres sont codés en dur dans indicators.py
+resdir=Resultats
+csv=$resdir/indicators.csv
+
+test -d $resdir || mkdir $resdir
+test -f $csv && mv -i $csv old-$csv
+echo 'identifiant ego;;nb amis;nb liens;nb com Louvain;nb amis max CC;nb com Louvain max CC;diametre;nb amis isoles;modularite;coeff clustering;densite' > $csv
 
 for ego in $data/????????????????????????????
 do
