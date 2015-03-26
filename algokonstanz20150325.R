c580cd1ce9139da9e2103528fb5ca960446b0096
Raphaël Charbey <r.charbey@liafa.fr>
2015-03-26 14:02:20 +0100

    modif setwdir code Mehwish

--- algokonstanz20150325.R
+++ algokonstanz20150325.R
@@ -88,8 +88,8 @@ for (findex in 171: length(fnames)){
     
     print(paste(workingDir,fnames[findex], sep="/"))
     
-    setwd(paste(workingDir,fnames[findex], sep="/"))
-    #setwd(paste(workingDir,fnames[findex], sep=""))
+    #setwd(paste(workingDir,fnames[findex], sep="/"))
+    setwd(paste(workingDir,fnames[findex], sep=""))
   ###############read statuses
   
   statusesFile <- readLines("statuses.jsons.gz")
