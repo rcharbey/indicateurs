231a29a8766eeda66d1e2290688d8874023d8529
Raphaël Charbey <r.charbey@liafa.fr>
2015-04-03 10:03:42 +0200

    modif script Mehwish

--- algokonstanz20150325.R
+++ algokonstanz20150325.R
@@ -73,8 +73,8 @@ library(arules)
 #how much error to create in the network
 pVal = 25
 
-workingDir = "/home/data/algopol/algopolapp/Raphael/Indicators/csa/"
-outputPath = "/home/data/algopol/algopolapp/Raphael/Indicators/Mehwish/newResults_pVal25/"
+workingDir = "/home/data/algopol/algopolapp/Raphael/Indicators/csa"
+outputPath = "/home/data/algopol/algopolapp/Raphael/Indicators/Mehwish/newResultspVal25/"
 # workingDir = "C:\\Users\\Mehwish\\Documents\\Link Prediction\\algopol-5egos-csa-nolinks\\algopol-5egos-csa-nolink-new\\Sample\\"
 # outputPath = "C:\\Users\\Mehwish\\Documents\\Link Prediction\\algopol-5egos-csa-nolinks\\algopol-5egos-csa-nolink-new\\newoutput\\"
 
